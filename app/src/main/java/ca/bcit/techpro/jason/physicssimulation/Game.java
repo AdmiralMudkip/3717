137d91919dd5534b82f8c69ab198836541f45975
limbermuffin <limbermuffin@gmail.com>
2016-02-11 21:42:00 -0800

    OnTouch for textureView

--- app/src/main/java/ca/bcit/techpro/jason/physicssimulation/Game.java
+++ app/src/main/java/ca/bcit/techpro/jason/physicssimulation/Game.java
@@ -2,9 +2,17 @@ package ca.bcit.techpro.jason.physicssimulation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.TextureView;
+import android.view.MotionEvent;
 import android.widget.TextView;
+import android.graphics.Point;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
 public class Game extends AppCompatActivity {
+    List<Point> pointList = new ArrayList<Point>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,11 +22,25 @@ public class Game extends AppCompatActivity {
         setContentView(R.layout.activity_game);
         TextView t = (TextView)findViewById(R.id.fullscreen_text);
         t.setText(s);
+        ((TextureView)findViewById(R.id.textureView)).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    pointList.add(new Point((int)event.getX(),(int)event.getY()));
+                    Toast.makeText(Game.this,"Touch coordinates : [" + (int) event.getX() + "," + (int)event.getY() + "]", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
     }
 
     private static String s = "medium";
 
     public static void setS(String a){
         s = a;
+    }
+
+    public void onClick(final View view){
+        System.out.println("goat");
     }
 }
