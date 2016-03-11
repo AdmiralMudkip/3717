347b40bc6bc826dda1ae9d88ece8614b4ceb2833
limbermuffin <limbermuffin@gmail.com>
2016-03-11 00:58:08 -0800

    Simplified some math
    Currently have issue with particles sticking to eachother. Intend to
    merge particles if they collide in future update.

--- app/src/main/java/ca/bcit/techpro/jason/physicssimulation/Game.java
+++ app/src/main/java/ca/bcit/techpro/jason/physicssimulation/Game.java
@@ -1,7 +1,6 @@
 package ca.bcit.techpro.jason.physicssimulation;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
@@ -9,22 +8,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
-import android.view.Display;
 import android.view.View;
-import android.view.TextureView;
 import android.view.MotionEvent;
 import android.widget.TextView;
-import android.graphics.Point;
-import android.widget.Toast;
 import android.graphics.Color;
-
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Game extends AppCompatActivity {
+    private static String s = "medium";
     private CanvasView cVas;
 
     @Override
@@ -37,8 +29,9 @@ public class Game extends AppCompatActivity {
         t.setText(s);
         cVas = (CanvasView) findViewById(R.id.canvas);
         Timer timer = new Timer();
-        timer.scheduleAtFixedRate(new Update(), 0, 17);
+        timer.scheduleAtFixedRate(new Update(), 0, 10);
     }
+
     class Update extends TimerTask {
         public void run() {
             runOnUiThread(new Runnable() {
@@ -50,38 +43,23 @@ public class Game extends AppCompatActivity {
             });
         }
     }
-    private static String s = "medium";
 
     public static void setS(String a){
         s = a;
     }
-
-    public void onClick(final View view){
-        System.out.println("goat");
-    }
 }
 
-
-
 class CanvasView extends View {
-    Particle[] particleArray = new Particle[16];
-    public int width;
-    public int height;
-    private Bitmap mBitmap;
-    private Canvas mCanvas;
+    Particle[] particleArray = new Particle[8];
     private Path mPath;
     Context context;
     private Paint mPaint;
-    private float mX, mY;
-    private static final float TOLERANCE = 5;
 
     public CanvasView(Context c, AttributeSet attrs) {
         super(c, attrs);
         context = c;
-
         // we set a new Path
         mPath = new Path();
-
         // and we set a new Paint with the desired attributes
         mPaint = new Paint();
         mPaint.setAntiAlias(true);
@@ -90,9 +68,8 @@ class CanvasView extends View {
         mPaint.setStrokeJoin(Paint.Join.ROUND);
         mPaint.setStrokeWidth(4f);
         DisplayMetrics metrics = context.getResources().getDisplayMetrics();
-        width = metrics.widthPixels;
-        height = metrics.heightPixels;
-
+        Particle.WIDTH = metrics.widthPixels;
+        Particle.HEIGHT = metrics.heightPixels;
     }
 
     // override onDraw
@@ -110,53 +87,19 @@ class CanvasView extends View {
         canvas.drawPath(mPath, mPaint);
     }
 
-    // when ACTION_DOWN start touch according to the x,y values
-    private void startTouch(int x, int y) {
-        for (int i = 0; i < particleArray.length; i++){
-            if (particleArray[i] == null) {
-                particleArray[i] = new Particle(x, y);
-                break;
-            }
-        }
-    }
-
     void physicsSim(){
-        for (int i = 0; i < particleArray.length; i++) {
-            for (int j = i+1; j < particleArray.length; j++) {
-                if (particleArray[i] != null && particleArray[j] != null) {
-                    double force = 10/Particle.distSq(particleArray[i], particleArray[j]);
-                    double angle = Particle.getAngle(particleArray[i], particleArray[j]);
-                    particleArray[i].xVel += force*Math.cos(angle);
-                    particleArray[i].yVel += force*Math.sin(angle);
-                    angle += 3.14159;
-                    particleArray[j].xVel += force*Math.cos(angle);
-                    particleArray[j].yVel += force*Math.sin(angle);
-                } else {
+        for (int i = 0; i < particleArray.length; i++)
+            for (int j = i+1; j < particleArray.length; j++)
+                if (particleArray[i] != null && particleArray[j] != null)
+                    Particle.updateVel(particleArray[i],particleArray[j]);
+                else
                     break;
-                }
-            }
-        }
-        for (int i = 0; i < particleArray.length; i++) {
-            if (particleArray[i] != null) {
-                if (particleArray[i].xPos > width) {
-                    particleArray[i].xPos = width;
-                    particleArray[i].xVel = 0;
-                } else if (particleArray[i].xPos < 0) {
-                    particleArray[i].xPos = 0;
-                    particleArray[i].xVel = 0;
-                }
-                if (particleArray[i].yPos > height) {
-                    particleArray[i].yPos = height;
-                    particleArray[i].yVel = 0;
-                } else if (particleArray[i].yPos < 0) {
-                    particleArray[i].yPos = 0;
-                    particleArray[i].yVel = 0;
-                }
+
+        for (int i = 0; i < particleArray.length; i++)
+            if (particleArray[i] != null)
                 particleArray[i].updatePos();
-            } else {
+            else
                 break;
-            }
-        }
     }
 
     //override the onTouchEvent
@@ -164,7 +107,12 @@ class CanvasView extends View {
     public boolean onTouchEvent(MotionEvent event) {
         invalidate();
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            startTouch((int) event.getX(), (int) event.getY());
+            for (int i = 0; i < particleArray.length; i++){
+                if (particleArray[i] == null) {
+                    particleArray[i] = new Particle(event.getX(), event.getY(), 1);
+                    break;
+                }
+            }
         }
         return true;
     }
--- app/src/main/java/ca/bcit/techpro/jason/physicssimulation/Particle.java
+++ app/src/main/java/ca/bcit/techpro/jason/physicssimulation/Particle.java
@@ -1,30 +1,55 @@
 package ca.bcit.techpro.jason.physicssimulation;
 
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.WindowManager;
+
 public class Particle {
+    static int G = 10;
+    static int WIDTH;
+    static int HEIGHT;
     public double xPos;
     public double yPos;
     public double xVel;
     public double yVel;
+    public int mass;
 
-    public Particle(double x, double y){
+    public Particle(double x, double y, int m){
         xPos = x;
         yPos = y;
+        mass = m;
+    }
+    public Particle(double x, double y, double xv, double yv, int m){
+        xPos = x;
+        xVel = xv;
+        yPos = y;
+        yVel = yv;
+        mass = m;
     }
 
-    public static double getDistance(Particle p1, Particle p2){
-        return Math.max(Math.sqrt(Math.pow(Math.abs(p1.xPos-p2.xPos),2)+Math.pow(Math.abs(p1.yPos-p2.yPos),2)),20);
-    }
 
-    public static double distSq(Particle p1, Particle p2){
-        return Math.max(Math.pow(Math.abs(p1.xPos-p2.xPos),2)+Math.pow(Math.abs(p1.yPos-p2.yPos),2)+1,400);
-    }
-
-    public static double getAngle(Particle p1, Particle p2){
-        return Math.atan2(p2.yPos - p1.yPos, p2.xPos - p1.xPos);
+    public static void updateVel(Particle p1, Particle p2){
+        double accel = G/Math.max((Math.pow(p1.xPos-p2.xPos,2)+Math.pow(p1.yPos-p2.yPos,2)), 900);
+        double angle = Math.atan2(p2.yPos - p1.yPos, p2.xPos - p1.xPos);
+        double xAccel = accel*Math.cos(angle);
+        double yAccel = accel*Math.sin(angle);
+        p1.xVel += xAccel * p2.mass;
+        p1.yVel += yAccel * p2.mass;
+        p2.xVel -= xAccel * p1.mass;
+        p2.yVel -= yAccel * p1.mass;
     }
 
     public void updatePos(){
+        if (xPos > WIDTH)
+            xVel = -.5*Math.abs(xVel);
+        else if (xPos < 0)
+            xVel = .5*Math.abs(xVel);
         xPos += xVel;
+
+        if (yPos > HEIGHT)
+            yVel = -.5*Math.abs(yVel);
+        else if (yPos < 0)
+            yVel = .5*Math.abs(yVel);
         yPos += yVel;
     }
 }
